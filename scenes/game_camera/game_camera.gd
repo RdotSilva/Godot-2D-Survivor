b447a0c5afe108c5ec5ba0a37ff03e013f0f5cdb
RdotSilva <RyandotSilva@gmail.com>
2024-10-19 12:05:17 +0100

    Refactor camera logic

--- scenes/game_camera/game_camera.gd
+++ scenes/game_camera/game_camera.gd
@@ -1,5 +1,6 @@
 extends Camera2D
 
+var target_position = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,8 +9,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	aquire_target()
+
+	# Formula used to create smooth "lerping"
+	var framerate_smoothing_formula = 1.0 - exp(-delta * 10)
+
+	global_position = global_position.lerp(target_position, framerate_smoothing_formula)
+	
+
+# Assign the target position only if the player exists
+func aquire_target():
 	var player_nodes = get_tree().get_nodes_in_group("player")
 
 	if player_nodes.size() > 0:
 		var player = player_nodes[0] as Node2D
-		global_position = player.global_position
+		target_position = player.global_position
