767fa6e9f9d938ed888db23d3c221fc1d5dc00b5
RdotSilva <RyandotSilva@gmail.com>
2024-10-21 18:31:06 +0100

    Add get_direction_to_player function

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func get_direction_to_player():
+	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+
+	if player_node != null:
+		return (player_node.global_position - global_position).normalized()
+		
+	return Vector2.ZERO
