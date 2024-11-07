e03275c8a7a49a363765212a4c294acf501b3a52
RdotSilva <RyandotSilva@gmail.com>
2024-11-07 17:45:46 +0000

    Add packed scene config

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+# Expose the variable in the inspector
+@export var basic_enemy_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
