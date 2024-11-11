bb3d9c3cede7f842266fc46dc0cf68c0e74bfe83
RdotSilva <RyandotSilva@gmail.com>
2024-11-11 19:05:23 +0000

    Add on_timer_timeout function

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -8,4 +8,25 @@ const SPAWN_RADIUS = 330
 # Called when the node enters the scene tree for the first time
 func _ready() -> void:
 	$Timer.timeout.connect(on_timer_timeout)
-	# TODO: Add on_timer_timeout function
+
+# Spawn the enemy outside the view of the player
+func on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+
+	if player == null:
+		return
+
+	# Get a random direction and rotate
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	
+	# Take player position with the direction
+	var spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+	# Create the node
+	var enemy = basic_enemy_scene.instantiate() as Node2D
+
+	# Add node to scene tree (under which parent)
+	get_parent().add_child(enemy)
+
+	# Assign global position for the enemy
+	enemy.global_position = spawn_position
