be81a2592914e931be2963f86ebef57e44fc4688
RdotSilva <RyandotSilva@gmail.com>
2024-11-11 19:03:05 +0000

    Add TODO for creating timer function

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,7 +1,11 @@
 extends Node
 
+# Viewport is 640 and half of that is 320 (+10 for buffer)
+const SPAWN_RADIUS = 330
+
 @export var basic_enemy_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
+# Called when the node enters the scene tree for the first time
 func _ready() -> void:
-	pass # Replace with function body.
+	$Timer.timeout.connect(on_timer_timeout)
+	# TODO: Add on_timer_timeout function
