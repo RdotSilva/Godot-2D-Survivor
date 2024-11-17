1221ade10ae37d49201851e520e769e6c1095b8a
RdotSilva <RyandotSilva@gmail.com>
2024-11-17 19:28:40 +0000

    Add node variable for timer

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# Shorthand for assigning a node to a variable when the node becomes ready
+@onready var timer = $Timer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
