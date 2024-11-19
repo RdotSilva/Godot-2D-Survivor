03166e09c24d0dc6317c0d0b05bf83baa246a9f6
RdotSilva <RyandotSilva@gmail.com>
2024-11-19 19:07:39 +0000

    Add placeholder ready function

--- scenes/loot/loot.gd
+++ scenes/loot/loot.gd
@@ -1,3 +1,8 @@
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var loot = generate_loot_pool()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
