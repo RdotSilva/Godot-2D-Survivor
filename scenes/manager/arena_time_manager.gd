fe91d5e2d4304f541eab2a4cc88fc99204080d9a
RdotSilva <RyandotSilva@gmail.com>
2024-11-25 19:16:34 +0000

    Remove unused functions

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -3,15 +3,6 @@ extends Node
 # Shorthand for assigning a node to a variable when the node becomes ready
 @onready var timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 # Get the total amount of time elapsed
 func get_time_elapsed():
