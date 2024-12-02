176fe8d4d967655708d745b9dfd0a985e83c7098
RdotSilva <RyandotSilva@gmail.com>
2024-12-02 18:55:39 +0000

    Add signal for experience vial collection

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -1,6 +1,7 @@
 extends Node
-class_name GameEvents
+
+signal experience_vial_collected(number: float)
 
 
 func emit_experience_vial_collected(number: float):
-	pass
+	experience_vial_collected.emit(number)
