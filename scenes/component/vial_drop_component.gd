6a09ee0c116aef94da108479b024f563999745c6
RdotSilva <RyandotSilva@gmail.com>
2024-12-08 23:09:05 +0000

    Add logic for vial drop percentage

--- scenes/component/vial_drop_component.gd
+++ scenes/component/vial_drop_component.gd
@@ -1,22 +1,26 @@
 extends Node
 
-@export var vial_scene: PackedScene
+@export_range(0, 1) var drop_percent: float = .5 # 50% drop rate that is configurable in UI
 @export var health_component: Node
+@export var vial_scene: PackedScene
 
 func _ready():
-    (health_component as HealthComponent).died.connect(on_died)
+	(health_component as HealthComponent).died.connect(on_died)
 
 
 func on_died():
-    if vial_scene == null:
-        return
+	if randf() > drop_percent:
+		return
 
-    if not owner is Node2D:
-        return
+	if vial_scene == null:
+		return
 
-    var spawn_position = (owner as Node2D).global_position
+	if not owner is Node2D:
+		return
 
-    var vial_instance = vial_scene.instantiate() as Node2D
+	var spawn_position = (owner as Node2D).global_position
 
-    owner.get_parent().add_child(vial_instance)
-    vial_instance.global_position = spawn_position
+	var vial_instance = vial_scene.instantiate() as Node2D
+
+	owner.get_parent().add_child(vial_instance)
+	vial_instance.global_position = spawn_position
