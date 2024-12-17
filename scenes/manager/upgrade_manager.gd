2cabf7c687c249182da64e3289641a140ac0bfbe
RdotSilva <RyandotSilva@gmail.com>
2024-12-17 19:31:02 +0000

    Set up script for level up logic

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -1,3 +1,12 @@
 extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
+@export var experience_manager: Node
+
+var current_upgrades = {}
+
+
+func _ready():
+	experience_manager.level_up.connect(on_level_up)
+
+
