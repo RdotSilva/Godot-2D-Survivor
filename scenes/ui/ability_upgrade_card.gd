f81e982acdfe2dd3ce742988e4637459f4ff6c19
RdotSilva <RyandotSilva@gmail.com>
2024-12-19 18:58:12 +0000

    Add variables for labels

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,4 +1,7 @@
 extends PanelContainer
 
+@onready var name_label: Label = $NameLabel
+@onready var description_label: Label = $DescriptionLabel
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
     pass
