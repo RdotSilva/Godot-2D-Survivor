3dd7fbb0f64c96258c585f04b83c86cd3e35d33b
RdotSilva <RyandotSilva@gmail.com>
2024-12-26 20:11:22 +0000

    Update logic for set_ability_upgrades

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -14,7 +14,8 @@ func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
         var card_instance = upgrade_card_scene.instantiate()
         card_container.add_child(card_instance)
         card_instance.set_ability_upgrade(upgrade)
+        card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
 
-# TODO: Implement upgrade selection logic
+# TODO: Implement upgrade selection
 func on_upgrade_selected(upgrade: AbilityUpgrade):
     pass
