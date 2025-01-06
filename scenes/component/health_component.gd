482ba63bbcdeb851517046f385c40e0162af5706
RdotSilva <RyandotSilva@gmail.com>
2025-01-06 20:20:28 +0000

    Add implementation for health bar

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -2,6 +2,7 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal health_changed
 
 @export var max_health: float = 10
 var current_health
@@ -13,6 +14,10 @@ func _ready():
 func damage(damage_amount: float):
 	# Health should never be negative
 	current_health = max(current_health - damage_amount, 0)
+
+	# Refactor this in future when adding health pots
+	health_changed.emit()
+
 	Callable(check_death).call_deferred()
 
 
@@ -20,6 +25,7 @@ func get_health_percent():
 	if max_health <= 0:
 		return 0
 	return min(current_health / max_health, 1)
+
 
 func check_death():
 	if current_health == 0:
