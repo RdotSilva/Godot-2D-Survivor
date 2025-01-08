7f52234ab463687e190feca4d1f139cb5b54eb2f
RdotSilva <RyandotSilva@gmail.com>
2025-01-08 20:15:56 +0000

    Add _ready function

--- scenes/ui/victory_screen.gd
+++ scenes/ui/victory_screen.gd
@@ -1,1 +1,7 @@
 extends CanvasLayer
+
+
+func _ready():
+    get_tree().paused = true
+    $%RestartButton.pressed.connect(on_restart_button_pressed)
+    $%QuitButton.pressed.connect(on_quit_button_pressed)
