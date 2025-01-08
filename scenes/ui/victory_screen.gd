e5400f3f31890feab935a6aa1cdd41ba916e2a59
RdotSilva <RyandotSilva@gmail.com>
2025-01-08 20:19:03 +0000

    Add on_restart_button_pressed function

--- scenes/ui/victory_screen.gd
+++ scenes/ui/victory_screen.gd
@@ -5,3 +5,8 @@ func _ready():
     get_tree().paused = true
     $%RestartButton.pressed.connect(on_restart_button_pressed)
     $%QuitButton.pressed.connect(on_quit_button_pressed)
+
+
+func on_restart_button_pressed():
+    # Change root node that is running the game
+    get_tree().change_scene_to_file("res://scenes/main/main.tscn")
