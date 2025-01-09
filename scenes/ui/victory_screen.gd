1df7d3eb3f04ed0341afb9fe9ec141d5bd32fee1
RdotSilva <RyandotSilva@gmail.com>
2025-01-09 20:45:21 +0000

    Add on_quit_button_pressed function

--- scenes/ui/victory_screen.gd
+++ scenes/ui/victory_screen.gd
@@ -10,3 +10,7 @@ func _ready():
 func on_restart_button_pressed():
     # Change root node that is running the game
     get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_quit_button_pressed():
+    get_tree().quit()
