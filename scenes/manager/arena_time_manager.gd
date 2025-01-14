20034298985ef21e7994a495106a8673d36b953b
RdotSilva <RyandotSilva@gmail.com>
2025-01-14 20:52:53 +0000

    Add override for _process function

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal arena_difficulty_increased
+
 const DIFFICULTY_INTERVAL = 5
 
 @export var end_screen_scene: PackedScene
@@ -12,8 +14,15 @@ var previous_time = 0
 
 
 func _ready():
-	previous_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta: float):
+	var next_time_target = timer.wait_time - ((arena_difficulty + 1) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit()
+		print(arena_difficulty)
 
 
 # Get the total amount of time elapsed
