489faf314b8fffbd7641d5d703d858d17711fe77
RdotSilva <RyandotSilva@gmail.com>
2025-01-22 19:55:15 +0000

    Add _ready function override

--- scenes/ability/axe_ability_controller/axe_ability_controller.gd
+++ scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -3,6 +3,10 @@ extends Node
 @export var axe_ability_scene: PackedScene
 
 
+func _ready():
+    $Timer.timeout.connect(on_timer_timeout)
+
+
 func on_timer_timeout():
     var player = get_tree().get_first_node_in_group("player") as Node2D
     
