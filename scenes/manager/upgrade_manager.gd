e970cabe2f0b53e6656b6dbd5569072fd3a1554a
RdotSilva <RyandotSilva@gmail.com>
2025-01-28 18:47:15 +0000

    Move on_level_up function

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -10,18 +10,6 @@ var current_upgrades = {}
 func _ready():
 	experience_manager.level_up.connect(on_level_up)
 
-
-func on_level_up(current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-
-	if chosen_upgrade == null:
-		return
-	
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
 
@@ -39,3 +27,15 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+
+func on_level_up(current_level: int):
+	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
+
+	if chosen_upgrade == null:
+		return
+	
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
