345108e2dc057c86c2d8219bada8efc9be2a22d8
RdotSilva <RyandotSilva@gmail.com>
2025-02-11 18:42:21 +0000

    Add logic to play death animation

--- scenes/component/death_component.gd
+++ scenes/component/death_component.gd
@@ -1,1 +1,22 @@
 extends Node2D
+
+@export var health_component: Node
+@export var sprite: Sprite2D
+
+
+func _ready():
+    # This will keep the particle in sync with the enemy visual
+    $GPUParticles2D.texture = sprite.text
+    
+    health_component.died.connect(on_died)
+
+func on_died():
+    # Get reference before we remove ourselves otherwise it won't be available
+    var entities = get_tree().get_first_node_in_group("entities_layer")
+
+    get_parent().remove_child(self)
+
+    entities.add_child(self)
+
+    $AnimationPlayer.play("default")
+
