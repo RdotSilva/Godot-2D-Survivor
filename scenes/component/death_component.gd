16f9acefcad0bb5dd1c45324ccafa0c428ed6525
RdotSilva <RyandotSilva@gmail.com>
2025-02-12 17:55:19 +0000

    Add logic to trigger death animation

--- scenes/component/death_component.gd
+++ scenes/component/death_component.gd
@@ -5,18 +5,24 @@ extends Node2D
 
 
 func _ready():
-    # This will keep the particle in sync with the enemy visual
-    $GPUParticles2D.texture = sprite.text
-    
-    health_component.died.connect(on_died)
+	# This will keep the particle in sync with the enemy visual
+	$GPUParticles2D.texture = sprite.texture
+	
+	health_component.died.connect(on_died)
 
 func on_died():
-    # Get reference before we remove ourselves otherwise it won't be available
-    var entities = get_tree().get_first_node_in_group("entities_layer")
+	if owner == null || not owner is Node2D:
+		return
 
-    get_parent().remove_child(self)
+	var spawn_position = owner.global_position
 
-    entities.add_child(self)
+	# Get reference before we remove ourselves otherwise it won't be available
+	var entities = get_tree().get_first_node_in_group("entities_layer")
 
-    $AnimationPlayer.play("default")
+	get_parent().remove_child(self)
 
+	entities.add_child(self)
+
+	global_position = spawn_position
+
+	$AnimationPlayer.play("default")
