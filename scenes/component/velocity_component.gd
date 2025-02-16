97cddafa7084536cad668a06db2a755c20f833b7
RdotSilva <RyandotSilva@gmail.com>
2025-02-16 20:13:39 +0000

    Add accelerate_in_direction function

--- scenes/component/velocity_component.gd
+++ scenes/component/velocity_component.gd
@@ -3,4 +3,14 @@ extends Node
 @export var max_speed: int = 40
 @export var acceleration: float = 5
 
-var velocity = Vector2.ZEROA
+var velocity = Vector2.ZERO
+
+
+
+
+func accelerate_in_direction(direction: Vector2):
+    var desired_velocity = direction * max_speed
+
+    velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
+
+
