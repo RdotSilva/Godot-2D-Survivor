d3ecb9923ab89e966e41d984c32ecfa5710a2668
RdotSilva <RyandotSilva@gmail.com>
2025-02-19 18:11:10 +0000

    Add movement logic

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,4 +1,14 @@
 extends CharacterBody2D
 
+@onready var velocity_component = $VelocityComponent
+@onready var visuals = $Visuals
+
+
 func _process(delta: float):
-    pass
+    velocity_component.accelerate_to_player()
+    velocity_component.move(self)
+
+    # Face enemy in correct location
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
