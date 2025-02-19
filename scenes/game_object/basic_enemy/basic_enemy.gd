cbfce93575808c3f34b384de8ccaf73d4b68c17c
RdotSilva <RyandotSilva@gmail.com>
2025-02-19 18:11:43 +0000

    Remove unused function

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -12,12 +12,3 @@ func _process(delta: float) -> void:
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(-move_sign, 1)
-
-
-func get_direction_to_player():
-	var player_node = get_tree().get_first_node_in_group("player") as Node2D
-
-	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
-		
-	return Vector2.ZERO
