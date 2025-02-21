8b63d17c79c6e40115896e09306edfe27408cc9a
RdotSilva <RyandotSilva@gmail.com>
2025-02-21 20:07:47 +0000

    Fix enemy face location

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	# Face enemy in correct location
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
-		visuals.scale = Vector2(-move_sign, 1)
+		visuals.scale = Vector2(move_sign, 1)
