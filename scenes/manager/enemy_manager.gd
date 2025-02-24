df37e70e1e5a26c8beae9eebabc1112517764bf8
RdotSilva <RyandotSilva@gmail.com>
2025-02-24 19:54:45 +0000

    Update basic enemy spawn to use new weighted table

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -10,9 +10,13 @@ const SPAWN_RADIUS = 370
 @onready var timer = $Timer
 
 var base_spawn_time = 0
+var enemy_table = WeightedTable.new()
+
 
 # Called when the node enters the scene tree for the first time
 func _ready() -> void:
+	enemy_table.add_item(basic_enemy_scene, 10)
+
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
@@ -57,8 +61,8 @@ func on_timer_timeout():
 	if player == null:
 		return
 
-	# Create the node
-	var enemy = basic_enemy_scene.instantiate() as Node2D
+	var enemy_scene = enemy_table.pick_item()
+	var enemy = enemy_scene.instantiate() as Node2D
 
 	# This should always exist
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
