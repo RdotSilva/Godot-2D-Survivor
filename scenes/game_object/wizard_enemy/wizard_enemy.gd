2017f3bf03be1467879baaff05635e8084218507
RdotSilva <RyandotSilva@gmail.com>
2025-02-25 18:44:22 +0000

    Add set_is_moving function

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,12 +3,23 @@ extends CharacterBody2D
 @onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
 
+var is_moving = false
+
 
 func _process(delta: float) -> void:
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
+
+	# Always apply the movement even if the movement or velocity stops
 	velocity_component.move(self)
 
 	# Face enemy in correct location
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(move_sign, 1)
+
+
+func set_is_moving(moving: bool):
+	is_moving = moving
