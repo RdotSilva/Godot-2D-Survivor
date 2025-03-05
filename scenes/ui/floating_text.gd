0072039b7558ce4b8710577322bf58ef510f1157
RdotSilva <RyandotSilva@gmail.com>
2025-03-05 19:28:53 +0000

    Add start function

--- scenes/ui/floating_text.gd
+++ scenes/ui/floating_text.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+func start(text: String):
+    $Label.text = text
+
+    var tween = create_tween()
+    tween.tween_property(self, "global_position", global_position + (Vector2.UP * 16), 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+    tween.tween_property(self, "global_position", global_position + (Vector2.UP * 48), 0.4).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
