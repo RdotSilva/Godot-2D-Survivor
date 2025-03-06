02febaf105dbef3dfb700047dfa9caebf7a4d672
RdotSilva <RyandotSilva@gmail.com>
2025-03-06 18:42:07 +0000

    Wire up floating text logic to hurt box

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -3,6 +3,9 @@ class_name HurtboxComponent
 
 @export var health_component: Node
 
+# Alternative way of loading a scene rather than using @export variable
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+
 
 func _ready():
     area_entered.connect(on_area_entered)
@@ -16,3 +19,8 @@ func on_area_entered(other_area: Area2D):
 
     var hitbox_component = other_area as HitboxComponent
     health_component.damage(hitbox_component.damage)
+
+    var floating_text = floating_text_scene.instantiate() as Node2D
+    get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+    floating_text.global_position = global_position
+    floating_text.start(str(hitbox_component.damage))
