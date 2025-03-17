20a719cd30e324dfdd6787567c7a95768b4fbdfc
RdotSilva <RyandotSilva@gmail.com>
2025-03-17 19:37:59 +0000

    Refactor pick_item function using exclude list

--- scripts/weighted_table.gd
+++ scripts/weighted_table.gd
@@ -9,12 +9,25 @@ func add_item(item, weight: int):
     weight_sum += weight
 
 
-func pick_item():
-    var chosen_weight = randi_range(1, weight_sum)
+func pick_item(exclude: Array = []):
+    var adjusted_items: Array[Dictionary] = items
+    var adjusted_weight_sum = weight_sum
 
+    # TODO: Look into improving the performance of this (may not scale with massive numbers of upgrades)
+    if exclude.size() > 0:
+        adjusted_items = []
+        adjusted_weight_sum = 0
+
+        for item in items:
+            if item["item"] in exclude:
+                continue
+            adjusted_items.append(item)
+            adjusted_weight_sum += item["weight"]
+
+    var chosen_weight = randi_range(1, adjusted_weight_sum)
     var iteration_sum = 0
 
-    for item in items:
+    for item in adjusted_items:
         iteration_sum += item["weight"]
         if chosen_weight <= iteration_sum:
             return item["item"]
