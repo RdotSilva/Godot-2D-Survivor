67da0eb933522a508170c1f9f9f297dcb374a5a2
RdotSilva <RyandotSilva@gmail.com>
2025-03-19 19:55:14 +0000

    Add on_ability_upgrade_added function

--- scenes/ability/axe_ability_controller/axe_ability_controller.gd
+++ scenes/ability/axe_ability_controller/axe_ability_controller.gd
@@ -7,7 +7,7 @@ var additional_damage_percent = 1
 
 func _ready():
     $Timer.timeout.connect(on_timer_timeout)
-    
+
     GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
@@ -26,3 +26,8 @@ func on_timer_timeout():
     foreground.add_child(axe_instance)
     axe_instance.global_position = player.global_position
     axe_instance.hitbox_component.damage = base_damage * additional_damage_percent
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+    if upgrade.id == "axe_damage":
+        additional_damage_percent = 1 + (current_upgrades["axe_damage"]["quantity"] * 0.10)
