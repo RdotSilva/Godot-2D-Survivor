f5f0416c6cdc0ca58d845110107cf65b78549d57
RdotSilva <RyandotSilva@gmail.com>
2025-04-19 17:40:44 +0100

    Add on_hit function

--- scenes/game_object/boss_enemy/boss_enemy.gd
+++ scenes/game_object/boss_enemy/boss_enemy.gd
@@ -16,3 +16,7 @@ func _process(delta: float) -> void:
 	var move_sign = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale = Vector2(-move_sign, 1)
+
+
+func on_hit():
+	$AudioStreamPlayer2D.play()
