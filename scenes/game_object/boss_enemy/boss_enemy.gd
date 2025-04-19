2bd7acd42864b5776a923eb0f40c50869bc3092b
RdotSilva <RyandotSilva@gmail.com>
2025-04-19 17:40:07 +0100

    Add override for process function

--- scenes/game_object/boss_enemy/boss_enemy.gd
+++ scenes/game_object/boss_enemy/boss_enemy.gd
@@ -6,3 +6,13 @@ extends CharacterBody2D
 
 func _ready():
 	$HurtBoxComponent.hit.connect(on_hit)
+
+    # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+
+	# Face enemy in correct location
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
