4bb3d5e9a6b776d1cc78810b9ec8e1efff6ebeb5
RdotSilva <RyandotSilva@gmail.com>
2025-04-27 19:17:51 +0100

    Add default randomized pitch

--- scenes/component/random_stream_player_2d_component.gd
+++ scenes/component/random_stream_player_2d_component.gd
@@ -10,5 +10,10 @@ func play_random():
     if streams == null || streams.size() == 0:
         return
 
+    if randomize_pitch:
+        pitch_scale = randf_range(min_pitch, max_pitch)
+    else:
+        pitch_scale = 1.0
+
     stream = streams.pick_random()
     play()
