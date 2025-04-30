e387bbe7be56e8274d9aaf0c0686e9eb250ce59e
RdotSilva <RyandotSilva@gmail.com>
2025-04-30 19:22:36 +0100

    Add exported variables used for config

--- scenes/component/random_stream_player_component.gd
+++ scenes/component/random_stream_player_component.gd
@@ -1,1 +1,6 @@
-extends AudioStreamPlayer2D
+extends AudioStreamPlayer
+
+@export var streams: Array[AudioStream]
+@export var randomize_pitch: bool = true
+@export var min_pitch: float = 0.9
+@export var max_pitch: float = 1.1
