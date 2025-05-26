bf1ba185c792d567d6b75ea85090b8aa8a749f04
RdotSilva <RyandotSilva@gmail.com>
2025-05-26 20:11:49 +0100

    Add ready override with tween logic

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -1,1 +1,11 @@
 extends CanvasLayer
+
+@onready var paneL_container = %PanelContainer
+
+
+func _ready():
+    $AnimationPlayer.play("default")
+
+    var tween = create_tween()
+    tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0)
+    tween.tween_property(paneL_container, "scale", Vector2.ONE, 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
