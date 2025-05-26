6c426979df00d846345ba1a1dc3e911581ce3ec2
RdotSilva <RyandotSilva@gmail.com>
2025-05-26 20:14:34 +0100

    Add resume button logic

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -4,8 +4,24 @@ extends CanvasLayer
 
 
 func _ready():
+    get_tree().paused = true
+    $%ResumeButton.pressed.connect(_on_resume_pressed)
+
     $AnimationPlayer.play("default")
 
     var tween = create_tween()
     tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0)
     tween.tween_property(paneL_container, "scale", Vector2.ONE, 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+
+
+func _on_resume_pressed():
+    $AnimationPlayer.play_backwards("default")
+
+    var tween = create_tween()
+    tween.tween_property(paneL_container, "scale", Vector2.ONE, 0)
+    tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0.3).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+
+    await tween.finished
+
+    get_tree().paused = false
+    queue_free()
