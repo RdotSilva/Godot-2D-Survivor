1907ba1379bb88e96a1fae4282ff1c6319bb3dc4
RdotSilva <RyandotSilva@gmail.com>
2025-05-28 19:04:23 +0100

    Add logic for options button press

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -2,52 +2,63 @@ extends CanvasLayer
 
 @onready var paneL_container = %PanelContainer
 
-
+var options_menu_scene = preload("res://scenes/ui/options_menu.tscn")
 var is_closing = false
 
 
 func _ready():
-    get_tree().paused = true
-    paneL_container.pivot_offset = paneL_container.size / 2
+	get_tree().paused = true
+	paneL_container.pivot_offset = paneL_container.size / 2
 
-    $%ResumeButton.pressed.connect(on_resume_pressed)
-    $%QuitButton.pressed.connect(on_quit_pressed)
-    
+	$%ResumeButton.pressed.connect(on_resume_pressed)
+	$%OptionsButton.pressed.connect(on_options_pressed)
+	$%QuitButton.pressed.connect(on_quit_pressed)
+	
 
-    $AnimationPlayer.play("default")
+	$AnimationPlayer.play("default")
 
-    var tween = create_tween()
-    tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0)
-    tween.tween_property(paneL_container, "scale", Vector2.ONE, 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	var tween = create_tween()
+	tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(paneL_container, "scale", Vector2.ONE, 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 
 func _unhandled_input(event):
-    if event.is_action_pressed("pause"):
-        close()
-        get_tree().root.set_input_as_handled()
+	if event.is_action_pressed("pause"):
+		close()
+		get_tree().root.set_input_as_handled()
 
 
 func close():
-    if is_closing:
-        return
+	if is_closing:
+		return
 
 
-    is_closing = true
-    $AnimationPlayer.play_backwards("default")
+	is_closing = true
+	$AnimationPlayer.play_backwards("default")
 
-    var tween = create_tween()
-    tween.tween_property(paneL_container, "scale", Vector2.ONE, 0)
-    tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0.3).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	var tween = create_tween()
+	tween.tween_property(paneL_container, "scale", Vector2.ONE, 0)
+	tween.tween_property(paneL_container, "scale", Vector2.ZERO, 0.3).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
 
-    await tween.finished
+	await tween.finished
 
-    get_tree().paused = false
-    queue_free()
+	get_tree().paused = false
+	queue_free()
 
 
 func on_resume_pressed():
-    close()
+	close()
+
+
+func on_options_pressed():
+	var options_menu_instance = options_menu_scene.instantiate()
+	add_child(options_menu_instance)
+	options_menu_instance.back_pressed.connect(on_options_back_pressed.bind(options_menu_instance))
 
 
 func on_quit_pressed():
-    get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+
+func on_options_back_pressed(options_menu: Node):
+	options_menu.queue_free()
