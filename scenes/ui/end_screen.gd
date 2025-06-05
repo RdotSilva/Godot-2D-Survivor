3e16a963f9f5a5b641e7f83d0cd668dec3555995
RdotSilva <RyandotSilva@gmail.com>
2025-06-05 18:40:15 +0100

    Add transition logic to restart button

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -28,7 +28,11 @@ func play_jingle(defeat: bool = false):
 		$VictoryStreamPlayer.play()
 
 func on_restart_button_pressed():
+	ScreenTransition.transition()
+	await ScreenTransition.transitioned_halfway
+
 	get_tree().paused = false
+
 	# Change root node that is running the game
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
