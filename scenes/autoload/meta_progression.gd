e917a25af71eb01487e88e7960cbe07bc343903d
RdotSilva <RyandotSilva@gmail.com>
2025-06-07 22:33:05 +0100

    Add upgrade currency logic

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,1 +1,14 @@
 extends Node
+
+var meta_upgrades: Dictionary = {
+    "meta_upgrade_currency": 0,
+    "meta_upgrades": {}
+}
+
+
+func _ready():
+    GameEvents.experience_vial_collected.connect(on_experience_collected)
+
+
+func on_experience_collected(number: float):
+    meta_upgrades["meta_upgrade_currency"] += number
