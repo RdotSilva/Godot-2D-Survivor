0ac9e49b1428b0f1a105a61fd6acce7724dd4e9b
RdotSilva <RyandotSilva@gmail.com>
2025-06-10 19:08:23 +0100

    Add ready override

--- scenes/ability/spear_ability/spear_ability.gd
+++ scenes/ability/spear_ability/spear_ability.gd
@@ -1,1 +1,19 @@
 extends Node2D
+
+# This is the furthest away the axe can be from the player
+const MAX_RADIUS = 100
+
+@onready var hitbox_component = $HitboxComponent
+
+var base_rotation = Vector2.RIGHT
+
+func _ready():
+    # Randomize the rotation
+    base_rotation = Vector2.RIGHT.rotated(randf_range(0, TAU))
+
+    # A tween is a way to define an animation
+    var tween = create_tween()
+    tween.tween_method(tween_method, 0.0, 2.0, 3)
+    tween.tween_callback(queue_free)
+
+
