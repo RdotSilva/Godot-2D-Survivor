595e6b91ad3819a098c91242dc5eacc7cb85ed5d
RdotSilva <RyandotSilva@gmail.com>
2025-06-11 19:32:15 +0100

    Add upgrade logic

--- scenes/ability/spear_ability_controller/spear_ability_controller.gd
+++ scenes/ability/spear_ability_controller/spear_ability_controller.gd
@@ -1,1 +1,13 @@
-extends Node2D
+extends Node
+
+@export var spear_ability_scene: PackedScene
+
+var base_damage = 1025
+var additional_damage_percent = 1
+
+func _ready():
+    GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+    if upgrade.id == "spear_damage":
+        additional_damage_percent = 1 + (current_upgrades["spear_damage"]["quantity"] * 0.10)
