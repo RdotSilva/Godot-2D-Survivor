804b54f4cd0546c296eaf1de5fd5257edda7184b
RdotSilva <RyandotSilva@gmail.com>
2025-06-22 17:04:19 +0100

    Revert card changes

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -19,13 +19,3 @@ func select_card():
 func on_gui_input(event: InputEvent):
 	if event.is_action_pressed("left_click"):
 		select_card()
-
-
-func on_upgrade_selected():
-	$AnimationPlayer.play("selected")
-	emit_signal("upgrade_selected", self)
-
-
-func on_card_closed():
-	$AnimationPlayer.play("closed")
-	emit_signal("card_closed", self)
