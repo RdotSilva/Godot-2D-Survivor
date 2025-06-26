fa5bb51f32d6547d5921933e645372d7e3da58f7
RdotSilva <RyandotSilva@gmail.com>
2025-06-26 17:37:51 +0100

    Add final logic to wire up card purchases

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -5,12 +5,12 @@ extends PanelContainer
 @onready var description_label: Label = $%DescriptionLabel
 @onready var progress_bar = %ProgressBar
 @onready var purchase_button = $%PurchaseButton
+@onready var progress_label = $%ProgressLabel
 
 var upgrade: MetaUpgrade
 
 func _ready():
 	purchase_button.pressed.connect(on_purchased_pressed)
-	gui_input.connect(on_gui_input)
 
 
 func set_meta_upgrade(upgrade: MetaUpgrade):
@@ -21,19 +21,16 @@ func set_meta_upgrade(upgrade: MetaUpgrade):
 
 
 func update_progress():
-	var percent = MetaProgression.save_data["meta_upgrade_currency"] / upgrade.experience_cost
+	var currency = MetaProgression.save_data["meta_upgrade_currency"]
+	var percent = currency / upgrade.experience_cost
 	percent = min(percent, 1)
 	progress_bar.value = percent
 	purchase_button.disabled = percent < 1
+	progress_label.text = str(currency) + "/" + str(upgrade.experience_cost)
 
 
 func select_card():
 	$AnimationPlayer.play("selected")
-
-
-func on_gui_input(event: InputEvent):
-	if event.is_action_pressed("left_click"):
-		select_card()
 
 
 func on_purchased_pressed():
@@ -41,3 +38,12 @@ func on_purchased_pressed():
 		return
 		
 	MetaProgression.add_meta_upgrade(upgrade)
+
+	# Subtract the cost from the currency
+	MetaProgression.save_data["meta_upgrade_currency"] -= upgrade.experience_cost
+	MetaProgression.save()
+
+	# Update progress on all MetaUpgradeCards using the group
+	get_tree().call_group("meta_upgrade_card", "update_progress")
+
+	$AnimationPlayer.play("selected")
