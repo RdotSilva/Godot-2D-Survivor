dfd9bf43dfef1c54ddcd4ce378759fcdf6a21b7b
RdotSilva <RyandotSilva@gmail.com>
2025-06-29 20:12:00 +0100

    Connect UpgradesButton on _ready function

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -4,6 +4,7 @@ var options_scene = preload("res://scenes/ui/options_menu.tscn")
 
 func _ready():
     $%PlayButton.pressed.connect(on_play_pressed)
+    $%UpgradesButton.pressed.connect(on_upgrades_pressed)
     $%OptionsButton.pressed.connect(on_options_pressed)
     $%QuitButton.pressed.connect(on_quit_pressed)
 
