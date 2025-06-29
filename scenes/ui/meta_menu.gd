df197934bf61c4c59b1fcdfaac39e7ae4f3437eb
RdotSilva <RyandotSilva@gmail.com>
2025-06-29 20:11:41 +0100

    Add logic to clear testing cards

--- scenes/ui/meta_menu.gd
+++ scenes/ui/meta_menu.gd
@@ -8,6 +8,10 @@ var meta_upgrade_card_scene = preload("res://scenes/ui/meta_upgrade_card.tscn")
 
 
 func _ready():
+    # This will allow us to keep 3 instances of the card for developer purposes and will clear them when the scene loads
+    for child in grid_container.get_children():
+        child.queue_free()
+
     for upgrade in upgrades:
         var meta_upgrade_card_instance = meta_upgrade_card_scene.instantiate()
         grid_container.add_child(meta_upgrade_card_instance)
