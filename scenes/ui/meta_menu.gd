8f1ecfbeec0d26500dce3ffd0631a5e0863bf231
RdotSilva <RyandotSilva@gmail.com>
2025-07-01 20:57:34 +0100

    Refactor on_back_pressed function using new helper

--- scenes/ui/meta_menu.gd
+++ scenes/ui/meta_menu.gd
@@ -17,3 +17,7 @@ func _ready():
         grid_container.add_child(meta_upgrade_card_instance)
 
         meta_upgrade_card_instance.set_meta_upgrade(upgrade)
+
+
+func on_back_pressed():
+    ScreenTransition.transition_to_scene("res://scenes/ui/main_menu.tscn")
