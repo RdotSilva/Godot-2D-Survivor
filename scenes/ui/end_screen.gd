a8d21985e617210972067de56a99da667b33c843
RdotSilva <RyandotSilva@gmail.com>
2025-07-02 20:34:43 +0100

    Replace logic for ContinueButton

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -11,7 +11,7 @@ func _ready():
 	tween.tween_property(paneL_container, "scale", Vector2.ONE, 0.3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 
 	get_tree().paused = true
-	$%RestartButton.pressed.connect(on_restart_button_pressed)
+	$%ContinueButton.pressed.connect(on_continue_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
 
 # Sets the title and description labels when the user is defeated
@@ -27,14 +27,14 @@ func play_jingle(defeat: bool = false):
 	else:
 		$VictoryStreamPlayer.play()
 
-func on_restart_button_pressed():
+func on_continue_button_pressed():
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
 
 	get_tree().paused = false
 
 	# Change root node that is running the game
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/ui/meta_menu.tscn")
 
 
 func on_quit_button_pressed():
