9e7c5ed1e1de4dfa4c722f8d19956585b3f54926
RdotSilva <RyandotSilva@gmail.com>
2025-07-31 18:59:01 +0100

    Add logic to handle multiple anvil spawns

--- scenes/ability/anvil_ability_controller/anvil_ability_controller.gd
+++ scenes/ability/anvil_ability_controller/anvil_ability_controller.gd
@@ -18,18 +18,25 @@ func on_timer_timeout():
         return
 
     var direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-    var spawn_position = player.global_position + (direction * randf_range(0, BASE_RANGE))
 
-     # Raycast collisions
-    var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
-    var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
-    if !result.is_empty():
-        spawn_position = result["position"]
+    # This is required when we have more than 1 anvil spawning
+    var additional_rotation_degrees = 360.0 / (anvil_count + 1)
 
-    var anvil_ability = anvil_ability_scene.instantiate()
-    get_tree().get_first_node_in_group("foreground_layer").add_child(anvil_ability)
-    anvil_ability.global_position = spawn_position
-    anvil_ability.hitbox_component.damage = BASE_DAMAGE
+    for i in anvil_count + 1:
+        var adjusted_direction = direction.rotated(deg_to_rad(i * additional_rotation_degrees))
+
+        var spawn_position = player.global_position + (adjusted_direction * randf_range(0, BASE_RANGE))
+
+        # Raycast collisions
+        var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+        var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+        if !result.is_empty():
+            spawn_position = result["position"]
+
+        var anvil_ability = anvil_ability_scene.instantiate()
+        get_tree().get_first_node_in_group("foreground_layer").add_child(anvil_ability)
+        anvil_ability.global_position = spawn_position
+        anvil_ability.hitbox_component.damage = BASE_DAMAGE
 
 
 func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
