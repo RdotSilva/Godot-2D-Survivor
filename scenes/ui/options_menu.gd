eea513709f07e5e19eb7cfac0b041c1f0a8e172e
RdotSilva <RyandotSilva@gmail.com>
2025-08-16 16:41:48 +0100

    Add TODO for save delete

--- scenes/ui/options_menu.gd
+++ scenes/ui/options_menu.gd
@@ -73,3 +73,8 @@ func on_back_pressed():
 # TODO: Add manual save functionality
 func on_save_pressed():
     pass
+
+
+# TODO: Add manual save functionality
+func on_delete_save_pressed():
+    pass
