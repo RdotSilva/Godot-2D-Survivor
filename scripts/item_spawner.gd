9806a5212369571f6782b09ab2bf5e8eb2beef30
Ryan Silva <31994025+RdotSilva@users.noreply.github.com>
2025-08-20 19:02:56 +0100

    Update item_spawner.gd
    Add add_item function

--- scripts/item_spawner.gd
+++ scripts/item_spawner.gd
@@ -4,3 +4,8 @@ class_name ItemSpawner
 
 var items: Array[Dictionary] = []
 var weight_sum = 0
+
+
+func add_item(item, weight: int):
+    items.append({"item": item, "weight": weight})
+    weight_sum += weight
