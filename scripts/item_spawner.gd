c1e06be2cd0af5c1a9e69ac72708ce37ff5a2821
Ryan Silva <31994025+RdotSilva@users.noreply.github.com>
2025-08-21 22:06:37 +0100

    Update item_spawner.gd
    Add pick_item function

--- scripts/item_spawner.gd
+++ scripts/item_spawner.gd
@@ -18,3 +18,26 @@ func remove_item(item_to_remove):
     for item in items:
         weight_sum += item["weight"]
 
+
+func pick_item(exclude: Array = []):
+    var adjusted_items: Array[Dictionary] = items
+    var adjusted_weight_sum = weight_sum
+
+    # TODO: Look into improving the performance of this (may not scale with massive numbers of upgrades)
+    if exclude.size() > 0:
+        adjusted_items = []
+        adjusted_weight_sum = 0
+
+        for item in items:
+            if item["item"] in exclude:
+                continue
+            adjusted_items.append(item)
+            adjusted_weight_sum += item["weight"]
+
+    var chosen_weight = randi_range(1, adjusted_weight_sum)
+    var iteration_sum = 0
+
+    for item in adjusted_items:
+        iteration_sum += item["weight"]
+        if chosen_weight <= iteration_sum:
+            return item["item"]
