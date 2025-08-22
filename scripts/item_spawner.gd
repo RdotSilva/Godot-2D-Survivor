eca3099b2b110685e51c804f91d0c88a926dc043
Ryan Silva <31994025+RdotSilva@users.noreply.github.com>
2025-08-22 20:57:15 +0100

    Update item_spawner.gd
    Refactor remove_item function

--- scripts/item_spawner.gd
+++ scripts/item_spawner.gd
@@ -18,7 +18,7 @@ func remove_item(item_to_remove):
     weight_sum = 0
 
     for item in items:
-        weight_sum += item["weight"]
+				increment_weight_sum(item["weight"])
 
 
 func pick_item(exclude: Array = []):
