c292230ef565a7178b953e87475b023d3fe33137
RdotSilva <RyandotSilva@gmail.com>
2025-10-22 17:58:46 +0100

    Add refund_all_upgrades function

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -59,6 +59,26 @@ func get_upgrade_count(upgrade_id: String):
 	return 0
 
 
+func refund_all_upgrades(upgrades: Array[MetaUpgrade]):
+	# Calculate total refund amount by iterating through all purchased upgrades
+	var total_refund = 0
+
+	for upgrade in upgrades:
+		if save_data["meta_upgrades"].has(upgrade.id):
+			var quantity = save_data["meta_upgrades"][upgrade.id]["quantity"]
+			total_refund += upgrade.experience_cost * quantity
+
+	# Reset all meta upgrades to empty
+	save_data["meta_upgrades"] = {}
+
+	# Add the refunded experience back to currency
+	save_data["meta_upgrade_currency"] += total_refund
+
+	save()
+
+	return total_refund
+
+
 func on_experience_collected(number: float):
 	save_data["meta_upgrade_currency"] += number
 	save()
