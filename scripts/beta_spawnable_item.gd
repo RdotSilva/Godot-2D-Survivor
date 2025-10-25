00778ff7babc56aa143d530d662ed24617507a81
Ryan Silva <31994025+RdotSilva@users.noreply.github.com>
2025-10-25 18:58:17 +0100

    Add bonus for spawned item

--- scripts/beta_spawnable_item.gd
+++ scripts/beta_spawnable_item.gd
@@ -2,5 +2,6 @@ class_name SpawnableItem
 extends Resource
 
 @export var scene: PackedScene
-@export var spawn_weight: float = 5.0
+@export var spawn_weight: float = 10
 @export var item_name: String = ""
+@export var bonus: float = 1
