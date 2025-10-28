227daea0013a8fc2d49184fac7e5f600fa733a1a
RdotSilva <RyandotSilva@gmail.com>
2025-10-28 19:24:07 +0000

    Remove beta scenes

--- scenes/ui/menus/meta_menu.gd
+++ scenes/ui/menus/meta_menu.gd
@@ -10,31 +10,31 @@ var meta_upgrade_card_scene = preload("res://scenes/ui/cards/meta_upgrade_card.t
 
 
 func _ready():
-    back_button.pressed.connect(on_back_pressed)
-    respec_button.pressed.connect(on_respec_pressed)
+	back_button.pressed.connect(on_back_pressed)
+	respec_button.pressed.connect(on_respec_pressed)
 
-    # This will allow us to keep 3 instances of the card for developer purposes and will clear them when the scene loads
-    for child in grid_container.get_children():
-        child.queue_free()
+	# This will allow us to keep 3 instances of the card for developer purposes and will clear them when the scene loads
+	for child in grid_container.get_children():
+		child.queue_free()
 
-    for upgrade in upgrades:
-        var meta_upgrade_card_instance = meta_upgrade_card_scene.instantiate()
-        grid_container.add_child(meta_upgrade_card_instance)
+	for upgrade in upgrades:
+		var meta_upgrade_card_instance = meta_upgrade_card_scene.instantiate()
+		grid_container.add_child(meta_upgrade_card_instance)
 
-        meta_upgrade_card_instance.set_meta_upgrade(upgrade)
+		meta_upgrade_card_instance.set_meta_upgrade(upgrade)
 
 
 func on_back_pressed():
-    ScreenTransition.transition_to_scene("res://scenes/ui/menus/main_menu.tscn")
+	ScreenTransition.transition_to_scene("res://scenes/ui/menus/main_menu.tscn")
 
 
 func on_respec_pressed():
-    # Check if player has any upgrades to refund
-    if MetaProgression.save_data["meta_upgrades"].is_empty():
-        return
+	# Check if player has any upgrades to refund
+	if MetaProgression.save_data["meta_upgrades"].is_empty():
+		return
 
-    # Refund all upgrades
-    MetaProgression.refund_all_upgrades(upgrades)
+	# Refund all upgrades
+	MetaProgression.refund_all_upgrades(upgrades)
 
-    # Update all upgrade cards to reflect the changes
-    get_tree().call_group("meta_upgrade_card", "update_progress")
+	# Update all upgrade cards to reflect the changes
+	get_tree().call_group("meta_upgrade_card", "update_progress")
