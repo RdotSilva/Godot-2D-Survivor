897a22a355d13e24531e473d00816921347712fe
RdotSilva <RyandotSilva@gmail.com>
2025-11-02 16:48:44 +0000

    Change to exponential experience gain system

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -3,7 +3,7 @@ extends Node
 signal experience_updated(current_experience: float, target_experience: float)
 signal level_up(new_level: int)
 
-const TARGET_EXPERIENCE_GROWTH = 5
+const XP_MULTIPLIER = 1.25 # Each level requires 25% more XP than the previous
 
 var current_experience = 0
 var current_level = 1
@@ -20,7 +20,7 @@ func increment_experience(number: float):
 
 	if current_experience == target_experience:
 		current_level += 1
-		target_experience += TARGET_EXPERIENCE_GROWTH
+		target_experience = max(1, int(target_experience * XP_MULTIPLIER))
 		current_experience = 0
 		experience_updated.emit(current_experience, target_experience)
 		level_up.emit(current_level)
