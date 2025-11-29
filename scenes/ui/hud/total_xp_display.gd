6d9efeee46659d6481575eb85c70a2dfb2026962
RdotSilva <RyandotSilva@gmail.com>
2025-11-29 18:43:33 +0000

    Add logic to update display

--- scenes/ui/hud/total_xp_display.gd
+++ scenes/ui/hud/total_xp_display.gd
@@ -1,1 +1,22 @@
 extends CanvasLayer
+
+var total_xp: float = 0
+@onready var label = $MarginContainer/Label
+
+
+func _ready():
+	# Reset total XP to 0 when scene loads (new match starts)
+	total_xp = 0
+	update_display()
+	
+	# Listen for experience vials being collected
+	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
+
+
+func on_experience_vial_collected(xp_amount: float):
+	total_xp += xp_amount
+	update_display()
+
+
+func update_display():
+	label.text = "Total XP: " + str(int(total_xp))
