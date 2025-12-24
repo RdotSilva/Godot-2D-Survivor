4a0cef62489ac90c81e052072c7f7344ef67c74c
RdotSilva <RyandotSilva@gmail.com>
2025-12-24 15:35:30 +0000

    Add increment_time function

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -29,6 +29,13 @@ func get_time_elapsed():
 	return timer.wait_time - timer.time_left
 
 
+# Increment the timer by the specified number of seconds (advances time)
+func increment_time(seconds: float):
+	if not timer.is_stopped():
+		var new_time_left = max(0.0, timer.time_left - seconds)
+		timer.start(new_time_left)
+
+
 func on_timer_timeout():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
